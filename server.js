459543594396c2941f429c2693c65a44cd23d088
Jared Love <lovejared91@yahoo.com>
2022-10-04 23:35:57 -0500

    added mysql connection and db connection

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 
+const mysql = require('mysql2');
+
+
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -11,11 +14,29 @@ app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
 
-app.listen(PORT, () => {
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'Callofduty@*4321',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
+
+  app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
 
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
 
 
 // Default response for any other request (Not Found)
